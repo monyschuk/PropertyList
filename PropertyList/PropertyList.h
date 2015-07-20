872235c8ddc049816ec94f65bd53e5052e6c98aa
Mark Onyschuk <mark.onyschuk@gmail.com>
2015-07-20 09:13:02 -0400

    Initial Commit

---
+++ PropertyList/PropertyList.h
@@ -0,0 +1,19 @@
+//
+//  PropertyList.h
+//  PropertyList
+//
+//  Created by Mark Onyschuk on 2015-07-20.
+//  Copyright © 2015 Mark Onyschuk. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for PropertyList.
+FOUNDATION_EXPORT double PropertyListVersionNumber;
+
+//! Project version string for PropertyList.
+FOUNDATION_EXPORT const unsigned char PropertyListVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PropertyList/PublicHeader.h>
+
+
